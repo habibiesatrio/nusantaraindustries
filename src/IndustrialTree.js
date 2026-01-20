556ffcc1b6f6d60fb84815441d98f33cd670aa19
Habibie Nugroho <32861853+habibiesatrio@users.noreply.github.com>
2026-01-20 20:29:25 +0700

    Delete src directory

--- src/IndustrialTree.js
+++
@@ -1,9 +0,0 @@
-// This file is redundant and no longer used in the application.
-// The functionality was moved to PohonIndustri.js to avoid a dependency issue.
-// This file can be safely deleted.
-
-const IndustrialTree = () => {
-  return null;
-};
-
-export default IndustrialTree;
--- src/db-config.js
+++
@@ -1,8 +0,0 @@
-const dbConfig = {
-    host: 'localhost',
-    user: 'u724103961_nusantaraid',
-    password: 'Pohonindustri2026!',
-    database: 'u724103961_Pohonindustri'
-};
-
-module.exports = dbConfig;

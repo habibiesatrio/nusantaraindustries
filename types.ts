b70af5937d0be58ee3acaa8251087180d79a1a36
Habibie Nugroho <32861853+habibiesatrio@users.noreply.github.com>
2026-01-22 06:27:57 +0700

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,34 @@
+
+export enum Page {
+  Landing = 'landing',
+  Login = 'login',
+  Dashboard = 'dashboard',
+  ExportImport = 'export-import',
+  InfoCenter = 'info-center',
+  AIConsultant = 'ai-consultant'
+}
+
+export interface CommodityDerivative {
+  level: string;
+  product: string;
+  valueAdded: string;
+}
+
+export interface CommodityData {
+  id: string;
+  name: string;
+  description: string;
+  importValue: number; // in Billion USD
+  exportValue: number; // in Billion USD
+  rawExport: number;
+  processedExport: number;
+  year: number;
+  derivatives: CommodityDerivative[];
+}
+
+export interface PSNProject {
+  name: string;
+  region: string;
+  status: string;
+  progress: number;
+}
